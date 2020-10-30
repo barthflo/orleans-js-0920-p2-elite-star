20f893d7096f2c20eef8f7c6ca76d23f7e9a472c
barthflo <flrnt.barth@gmail.com>
2020-10-30 22:44:58 +0100

    fixed header and modified top arrow behaviour

--- src/components/Header/Nav/Nav.jsx
+++ src/components/Header/Nav/Nav.jsx
@@ -7,8 +7,8 @@ import './Nav.css';
 
 function Nav() {
     return (
-      <div className="Nav pt-4 w-100">
-        <ul className="w-100 d-none d-md-inline-flex justify-content-around">
+      <div className="Nav pt-4 w-100 col-md-4">
+        <ul className="w-100 list-unstyled align-items-center d-flex flex-column flex-md-row d-md-inline-flex justify-content-around">
           <li><Link to="/">Home</Link></li>
           <li><Link to="/models">Models</Link></li>
           <li><Link to="/about">About</Link></li>
