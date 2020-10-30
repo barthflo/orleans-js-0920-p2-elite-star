96dbfeec4254a2d7639cdfe8d43154428036f8ab
AbiEmmanuel <abiakreemmanuel@gmail.com>
2020-10-30 10:30:19 +0100

    new articles

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FaGalacticSenate } from 'react-icons/fa';
 import { GiLightSabers } from 'react-icons/gi';
+import Articles from './components/Articles/Articles'; 
+
 
 function App() {
   return (
     <main className="container d-flex flex-column justify-content-center align-items-center">
-      <h1>Hello EliteStar !</h1>
-      <p>Let&apos;s start coding!</p>
-      <div>
-        <FaGalacticSenate className="icon color-cold" />
-        <GiLightSabers className="icon color-warm" />
-      </div>
+    
+      <Articles />
     </main>
   )
 };
