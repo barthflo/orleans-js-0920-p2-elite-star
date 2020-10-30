fefc4260bc5de2d6a550f3792dbbe23dff4c22e2
barthflo <flrnt.barth@gmail.com>
2020-10-30 19:48:07 +0100

    modified footer structure

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,10 @@
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-=======
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
-
->>>>>>> 9156d520d81385a464abd156640895dc38149bb5
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-<<<<<<< HEAD
-  document.querySelector('#root')
-);
-
-
-=======
   document.getElementById('root')
 );
->>>>>>> 9156d520d81385a464abd156640895dc38149bb5
