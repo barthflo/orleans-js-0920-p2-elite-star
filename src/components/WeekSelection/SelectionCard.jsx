3dff63b15ae2f948a43c907630622eb518179756
lolarne <rne.lola@gmail.com>
2020-11-04 13:58:15 +0100

    add styles

--- src/components/WeekSelection/SelectionCard.jsx
+++ src/components/WeekSelection/SelectionCard.jsx
@@ -2,10 +2,10 @@ import './WeekSelection.css';
 import RandomText from './RandomText';
 
 
-const SelectionCard = ({ name, id, image, species, eyeColor, hairColor, bornLocation }) => {
+const SelectionCard = ({ name, image, species, eyeColor, hairColor, bornLocation }) => {
     return (
         <div className="cardSelection">
-            <h3>{name}/ ID: {id}</h3>
+            <h3>{name}</h3>
             <figure><img className="img" src={image} alt={name} /></figure>
             <p>{name} a {species} {bornLocation ? (<p>from {bornLocation}</p>) : ""} knew how to combine {eyeColor} eyes and {hairColor ? hairColor : "no"} hair.</p>
             <p><RandomText/></p>
