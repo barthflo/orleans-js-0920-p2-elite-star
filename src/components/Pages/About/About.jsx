f1e5af84327831322abafc59376c0b26960d4312
me <mo.mokaddemini@gmail.com>
2020-11-11 16:57:55 +0100

    pull last version 10/11/2020

--- src/components/Pages/About/About.jsx
+++ src/components/Pages/About/About.jsx
@@ -2,8 +2,14 @@ import './About.css';
 
 function About() {
     return (
-        <div className="about d-flex justify-content-center align-items-center">
-            <h2>Page About</h2>
+        <div className="about d-flex flex-column justify-content-right align-items-center">
+            <div className='about-mask'></div>
+            <h2>ELITE STAR AGENCY</h2>
+            <p>19 Avenue George V</p>
+            <p>75008 Paris - France</p>
+            <p>T: +33 1 4044 3222</p>
+            <p>F: +33 1 4044 3280</p>
+            <p>E: info@elitemodel.fr</p>
         </div>
     )
 }
