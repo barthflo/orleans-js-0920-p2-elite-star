b1c3b91dd3435d6013dfb03a3d3a12c0636c8676
me <mo.mokaddemini@gmail.com>
2020-11-12 17:55:17 +0100

    adress_presentation

--- src/components/Pages/About/About.jsx
+++ src/components/Pages/About/About.jsx
@@ -2,14 +2,27 @@ import './About.css';
 
 function About() {
     return (
-        <div className="about d-flex flex-column justify-content-right align-items-center">
-            <div className='about-mask'></div>
-            <h2>ELITE STAR AGENCY</h2>
-            <p>19 Avenue George V</p>
-            <p>75008 Paris - France</p>
-            <p>T: +33 1 4044 3222</p>
-            <p>F: +33 1 4044 3280</p>
-            <p>E: info@elitemodel.fr</p>
+        <div className="about d-flex flex-column">
+            {/* <div className='about-mask'></div> */}
+                <h2 className='about-title'>ABOUT US</h2>
+            <div className='about-text'>
+                <div className='about-adress'>
+                    <h4>ELITE STAR AGENCY</h4>
+                    <h5>Coruscant / Alderaan</h5>
+                    <p>Keizersgracht 174-11016 DW</p> 
+                    <p>Sor-Akai Coruscan</p>
+                    <p>Coruscant - Galactic Empire</p>
+                    <p>T: 0679497610</p>
+                    <p>F: 0679497611</p>
+                    <p>E: elitestar@dark.rep</p>
+                </div>
+                <div className='description'>
+                    <p className='desc-title'>EliteStar was created to meet two objectives :</p>
+                    <p>- Represent models whose physical profiles may or may not correspond to the usual criteria of modeling agencies.</p>
+                    <p>- Allow event professionals to find the characters they need, namely profiles that are increasingly out of the ordinary and atypical.</p>
+                    <p>EliteStar agency is a member of the Intergalactic Federation of Modeling Agencies.</p>
+                </div>
+            </div>
         </div>
     )
 }
