06500f8e1fc6da68ed4d3ee7343906e855177436
me <mo.mokaddemini@gmail.com>
2020-11-12 18:05:31 +0100

    add modif

--- src/components/Pages/About/About.jsx
+++ src/components/Pages/About/About.jsx
@@ -3,20 +3,9 @@ import Faq from './Faq/Faq';
 
 function About() {
     return (
-<<<<<<< HEAD
-        <div className="about d-flex flex-column justify-content-right align-items-center">
-            <div className='about-mask'></div>
-            <h2>ELITE STAR AGENCY</h2>
-            <p>19 Avenue George V</p>
-            <p>75008 Paris - France</p>
-            <p>T: +33 1 4044 3222</p>
-            <p>F: +33 1 4044 3280</p>
-            <p>E: info@elitemodel.fr</p>
-=======
         <div className="about d-flex justify-content-center align-items-center">
             
             <Faq />
->>>>>>> 42fa73428c085c11eb3fb7f9e140565a1b4e1dd7
         </div>
     )
 }
