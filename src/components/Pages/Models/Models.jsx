581d5c7ea0289ed97f6705300dceb00b15df4f01
barthflo <flrnt.barth@gmail.com>
2020-11-16 17:20:04 +0100

    Toggle sides on models page

--- src/components/Pages/Models/Models.jsx
+++ src/components/Pages/Models/Models.jsx
@@ -1,20 +1,41 @@
 import ModelCard from './ModelCard/ModelCard';
 import './Models.css';
-
+import ToggleSide from '../ToggleSide/ToggleSide';
+import {ToggleSideContext} from '../ToggleSide/ToggleSide';
+import {useState} from 'react';
+import {light, dark} from '../../../App';
 
 const Models =() =>{
     const characters = JSON.parse(localStorage.getItem('characters'));
-    
+    const [activeTab, setActive] = useState("1");
+    const toggleActive =(tab) => {
+        activeTab !== tab && setActive(tab);  
+    }
+    const filtering = () => {
+        let filtered=[];
+        switch(activeTab){
+            case "2":
+                filtered = characters.filter(character => light.some(affiliation => character.affiliations.includes(affiliation))&& !dark.some(affiliation =>character.affiliations.includes(affiliation)));
+                return filtered;
+            case "3":
+                filtered = characters.filter(character => dark.some(affiliation => character.affiliations.includes(affiliation)) && !light.some(affiliation =>character.affiliations.includes(affiliation)));
+                return filtered;
+            default:
+                return characters;
+        } 
+    }
     return (
         <main className="models col-12 col-lg-10 offset-lg-1 px-0 py-md-5 px-md-4">
             <h1>Our Models</h1>
-            
-            <div className="model mt-5">
-                {characters.sort((a, b)=> 0.5 - Math.random())
-                           .map((character, index) => <ModelCard {...character} key={index} />)
-                           .reverse()
-                           }
-            </div>
+            <ToggleSideContext.Provider value={{activeTab:activeTab, toggleActive : toggleActive}}>
+                <ToggleSide />
+                <div className="model mt-5">
+                    {filtering().sort((a, b)=> 0.5 - Math.random())
+                            .map((character, index) => <ModelCard {...character} key={index} />)
+                            .reverse()
+                            }
+                </div>
+            </ToggleSideContext.Provider>
         </main>
     )
 }
---
+++ src/components/Pages/ToggleSide/ToggleSide.jsx
@@ -0,0 +1,18 @@
+import {createContext , useContext}from 'react';
+import classnames from 'classnames';
+import './ToggleSide.css';
+
+export const ToggleSideContext = createContext(null);
+
+function ToggleSide(props) {
+    const {toggleActive, activeTab}= useContext(ToggleSideContext);
+    return (
+        <ul className="toggle-side pagination">
+            <li onClick={() => toggleActive("1") } className={"page-link " + classnames({active : activeTab ==="1"})}>All</li>
+            <li onClick={() => toggleActive("2") } className={"page-link " + classnames({active : activeTab ==="2"})}>LightSide</li>
+            <li onClick={() => toggleActive("3") } className={"page-link " + classnames({active : activeTab ==="3"})}>DarkSide</li>
+        </ul>
+    )
+}
+
+export default ToggleSide
