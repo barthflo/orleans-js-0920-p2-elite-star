03bea44cc69cd83b9d61c89df34e1c22d40f5db5
barthflo <flrnt.barth@gmail.com>
2020-11-16 12:31:05 +0100

    Finished fix

--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -3,10 +3,10 @@ import logoMinimal from '../../../assets/LogoMobile.png';
 import {Link } from "react-router-dom";
 import './Logo.css';
 
-function Logo() {
+function Logo({useWindowSize}) {
     return (
-        <div className="Logo pb-md-4 mt-1">
-            <Link to="/"><img src={ (window.innerWidth <768) ? logoMinimal :  logo} alt="logo" /></Link>
+        <div className="Logo p-2 pb-md-4 mt-1">
+            <Link to="/"><img src={ (useWindowSize() <768) ? logoMinimal :  logo} alt="logo" /></Link>
         </div>
     )
 }
--- src/components/Header/Nav/Nav.jsx
+++ src/components/Header/Nav/Nav.jsx
@@ -1,17 +1,21 @@
 
 import {Link } from "react-router-dom";
 import './Nav.css';
+import {CgMenuGridO} from "react-icons/cg";
+import {useState} from 'react';
 
-
-
-function Nav() {
+function Nav({useWindowSize}) {
+    const [open, setOpen] = useState(false);
+    const toggle = () => setOpen(!open);
+    console.log(open);
     return (
-      <div className="Nav pt-4 w-100 col-1 col-sm-2 col-md-4 col-lg-6">
-        <ul className="w-100 list-unstyled align-items-center d-flex flex-column flex-md-row d-md-inline-flex justify-content-around">
+      <div onClick={toggle} className="Nav pt-md-4 w-100 col-2 col-sm-1 col-md-4 col-lg-6">
+        <ul className={"w-100 mb-0 mb-md-3 list-unstyled align-items-center flex-column py-4 py-md-0 flex-md-row d-md-inline-flex justify-content-around" + (open ? " d-flex" : " collapse")}>
           <li className="my-1"><Link to="/">Home</Link></li>
           <li className="my-1"><Link to="/models">Models</Link></li>
           <li className="my-1"><Link to="/about">About</Link></li>
-        </ul>   
+        </ul>
+        { useWindowSize()<768 && <CgMenuGridO onClick={toggle} color={"white"} size={"2em"} />}   
       </div>
     );
   }
